00055e486b3be8d79b73acaf6230c9bffdc76a3b
noua026 <noua026@gmail.ocm>
2024-07-24 17:31:07 +0900

    수정

--- full-stack1/starbucks-hompage/src/main/webapp/js/notice_admin.js
+++ full-stack1/starbucks-hompage/src/main/webapp/js/notice_admin.js
@@ -5,8 +5,3 @@ function regSubmit() {
 	// form태그 안의 action값으로 이동
 	document.getElementById('form1').submit();	
 }
-
-function regSubmit2() {
-	// form태그 안의 action값으로 이동
-	document.getElementById('form2').submit();	
-}
