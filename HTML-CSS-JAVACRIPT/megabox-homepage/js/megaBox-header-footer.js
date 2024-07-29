393a566a26251d652279628c534da5c5978be764
noua026 <noua026@gmail.ocm>
2024-07-29 10:09:34 +0900

    megabox수정

--- HTML-CSS-JAVACRIPT/megabox-homepage/js/megaBox-header-footer.js
+++ HTML-CSS-JAVACRIPT/megabox-homepage/js/megaBox-header-footer.js
@@ -47,3 +47,27 @@ document.querySelector("#login").addEventListener('click',function(){
   document.querySelector(".header-wrapper").style.display='none';
   supListContents.forEach(it=>it.style.display='none');
 })
+
+function showMenu() {
+  const menuEl = document.querySelector('footer .sub-menu');
+
+  function Menu(element) {
+      element.style.display = 'block';
+  }
+
+  Menu(menuEl);
+
+  window.scrollBy(0, -500);
+}
+
+function closeMenu() {
+  const menuEl = document.querySelector('footer .sub-menu');
+
+  function quit(star) {
+      star.style.display = 'none';
+  }
+
+  quit(menuEl);
+
+  window.scrollTo(0, document.body.scrollHeight);
+}
